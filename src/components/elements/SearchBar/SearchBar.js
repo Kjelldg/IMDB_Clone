2f12f90a79d0faacb68a03b592c7ad66075649f5
kjelldg <Kjelldg@gmail.com>
2018-10-14 20:32:17 +0200

    Initial commit.

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    state = {
+
+    }
+    render () {
+            return (
+            <div>
+            Searchbar
+            </div>
+            )
+    }
+
+}
+
+export default SearchBar;
