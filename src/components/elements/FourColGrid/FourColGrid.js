d4fd35bd8ddff46587635af46567a26a08b565fc
kjelldg <Kjelldg@gmail.com>
2018-10-14 20:52:18 +0200

    Scaffolding of website complete.

---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './FourColGrid.css';
+
+const FourColGrid = (props) => {
+    return (
+
+        <div>
+            FourColGrid
+        </div>
+
+
+    )
+}
+
+export default FourColGrid;
