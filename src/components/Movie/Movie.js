4a7ed1d7bfc33de82c6d503f72bc90a0fe6c4b6c
kjelldg <Kjelldg@gmail.com>
2018-10-17 19:44:12 +0200

    Thumbnails now clickable, header logos lead somewhere.

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { API_URL, API_KEY } from "../../config"
+//import Navigation from '../elements/Navigation/Navigation';
+
+class Movie extends Component {
+
+    render() {
+        return(
+            <div>
+                Movie
+            </div>
+        )
+    }
+}
+
+export default Movie;
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Whoops, this page does not exist.</h1>
+        </div>
+
+
+
+    )
+}
+
+export default NotFound;
