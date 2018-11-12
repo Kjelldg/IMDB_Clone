e3e0e7b650057e805ca34e5e39afc4cbb3c9db4a
kjelldg <Kjelldg@gmail.com>
2018-11-12 20:58:19 +0100

    Clarified a few things.

--- src/components/elements/NotFound/NotFound.js
+++ src/components/elements/NotFound/NotFound.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Whoops, this page does not exist.</h1>
-        </div>
-
-
-
-    )
-}
+  return (
+    <div>
+      <h1>Error 404:</h1>
+      <h1>Whoops, this page does not exist.</h1>
+    </div>
+  );
+};
 
 export default NotFound;
